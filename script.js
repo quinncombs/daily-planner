35325d0415da0cf5a2059552133932ee5325b92a
quinncombs <qcombs31@gmail.com>
2023-09-24 22:33:52 -0400

    fixed dayjs function

--- script.js
+++ script.js
@@ -1,75 +1,54 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
+//Defined variables
 var eventText;
 var eventTime;
 
-
   //Add code to display the current date in the header of the page.
-  var time = dayjs().format('hh:mm:ss');
-  $("#currentDay").text(time);
+
+// var time = dayjs().format('hh:mm:ss');
+//   $("#currentDay").text(time);
 
 $(document).ready(function() {
   colorChange();
-  // changeText();
+  saveText();
 
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO:  HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-  
   });
 
 // colors will change according to the time
   function colorChange () {
     
-    var currentDay = time;
+    var currentDay = dayjs();
     console.log("Current Time" + currentDay);
   
 
   //Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour.
-    $(".input").each(function () {
+    $(".input").each(function() {
       var scheduleTime = parseInt($(this).attr("id"));
       console.log(scheduleTime);
 
       if (currentDay > scheduleTime) {
-
+          //add or remove classes to apply styles
           $(this).removeClass("future");
           $(this).removeClass("present");
-          $(this).addClass(".past");
+          $(this).addClass("past");
 
       } else if (currentDay < scheduleTime) {
 
           $(this).removeClass("present");
-          $(this).removeClass(".past");
+          $(this).removeClass("past");
           $(this).addClass("future");
 
       } else {
           $(this).removeClass("future");
-          $(this).removeClass(".past");
+          $(this).removeClass("past");
           $(this).addClass("present");
 
       }
     })
 };
   
-  // save button functionality, locally save
+  // W3Schools: .val or value property sets or returns the value attribute of a text field. 
+  // Save button functionality, locally save
 $(".saveBtn").click(function() {
   eventText = $(this).siblings(".input").val();
   console.log(eventText);
@@ -78,6 +57,46 @@ $(".saveBtn").click(function() {
   localStorage.setItem(eventTime, JSON.stringify(eventText));
 
   colorChange ();
-  renderText ();
+  saveText ();
   
 });
+
+function saveText() {   
+
+  var saveEventText9 = JSON.parse(localStorage.getItem("9:00AM"));
+  $("#9").val("");
+  $("#9").val(saveEventText9);
+  
+  var saveEventText10 = JSON.parse(localStorage.getItem("10:00AM"));
+  $("#10").val("");
+  $("#10").val(saveEventText10);
+  
+  var saveEventText11 = JSON.parse(localStorage.getItem("11:00AM"));
+  $("#11").val("");
+  $("#11").val(saveEventText11);
+  
+  var saveEventText12 = JSON.parse(localStorage.getItem("12:00PM"));
+  $("#12").val("");
+  $("#12").val(saveEventText12);
+  
+  var saveEventText1 = JSON.parse(localStorage.getItem("1:00PM"));
+  $("#13").val("");
+  $("#13").val(saveEventText1);
+
+  var saveEventText2 = JSON.parse(localStorage.getItem("2:00PM"));
+  $("#14").val("");
+  $("#14").val(saveEventText2);
+
+  var saveEventText3 = JSON.parse(localStorage.getItem("3:00PM"));
+  $("#15").val("");
+  $("#15").val(saveEventText3);
+
+  var saveEventText4 = JSON.parse(localStorage.getItem("4:00PM"));
+  $("#16").val("");
+  $("#16").val(saveEventText4);
+
+  var saveEventText5 = JSON.parse(localStorage.getItem("5:00PM"));
+  $("#17").val("");
+  $("#17").val(saveEventText5);
+
+};
